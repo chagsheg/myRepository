367e5bdd1f59f2b84ede8d03eec8be155a241493
changsheng <1420952288@qq.com>
2021-05-19 21:49:42 +0800

    cs_branch commit1 1fq 1111

--- zhilian_entp_spider/start.py
+++ zhilian_entp_spider/start.py
@@ -8,6 +8,8 @@
 #-------------------------------------------------------------------------------
 import scrapy.cmdline as cmdline
 # 启动 爬虫
+
+dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,dev_cs,,
 dev   dsffsd
 cmdline.execute('scrapy crawssssl zhilian_entp_spider'.split())s s s s
 fds fsd sd
